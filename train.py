ffb22276d7dfd7c19bf6f1eaba43683625e93f83
minhup <minh.truonghong@gmail.com>
2017-04-21 16:12:00 +0200

    model + train

---
+++ train.py
@@ -0,0 +1,130 @@
+import numpy as np
+import tensorflow as tf
+import keras
+import os
+
+from keras.optimizers import Adam
+
+from fully_conv_model_for_lidar import fcn_model
+from util_func import *
+
+
+
+def list_of_data(data_dir):
+	list_of_lidar = []
+	list_of_gtbox = []
+	for f in os.listdir(data_dir):
+	    path = os.path.join(data_dir, f)
+	    lidar_path = os.path.join(path, 'lidar')
+	    gtbox_path = os.path.join(path, 'gt_boxes3d')
+	    num_files = len(os.listdir(lidar_path))
+	    
+	    lidar = [os.path.join(lidar_path, 'lidar_'+str(i)+'.npy') for i in range(num_files) ]
+	    gtbox = [os.path.join(gtbox_path, 'gt_boxes3d_'+str(i)+'.npy') for i in range(num_files) ]
+	    list_of_lidar += lidar
+	    list_of_gtbox += gtbox
+	return list_of_lidar, list_of_gtbox
+
+def data_generator(list_of_lidar, list_of_gtbox):
+    '''
+    input: list_of_lidar, list_of_gtbox
+    output: generator of lidar and gtbox
+    '''
+    n_sample = len(list_of_lidar)
+    next_epoch = True
+
+    while True:
+        if next_epoch:
+            indices = np.arange(n_sample)
+            np.random.shuffle(indices)
+            yield list_of_lidar[indices[0]], list_of_gtbox[indices[0]]
+            ind = 1
+            next_epoch = False
+        else:
+            yield list_of_lidar[indices[0]], list_of_gtbox[indices[0]]
+            ind += 1
+            if ind >= n_sample:
+                next_epoch = True  
+
+
+
+
+def train_batch_generator(list_of_lidar, list_of_gtbox, batch_size = 4, 
+                    data_augmentation = True, width = 256, height = 64):
+    '''
+    '''
+    theta_offset_range = 10*np.pi/180
+    ind = 0
+    for lidar_file, box_file in data_generator(list_of_lidar, list_of_gtbox):
+        lidar = np.load(lidar_file)
+        gt_box = np.load(box_file)
+        
+        if ind == 0:
+            batch_sample = np.zeros((batch_size, height, width, 2))
+            batch_label = np.zeros((batch_size, height, width, 10))
+            
+        if data_augmentation:
+        # Randomly flip the frame
+            flip = np.random.randint(1)
+            theta = np.random.uniform(low=-theta_offset_range, high=theta_offset_range)
+
+            lidar, gt_box = augmentation(theta, flip, lidar, gt_box)
+        
+        view, box = cylindrical_projection_with_labels(lidar, gt_box)
+
+        batch_sample[ind] = view
+        #batch_seg[ind] = seg
+        #batch_reg[ind] = reg
+        batch_label[ind] = box
+        
+        ind += 1
+        
+        if ind == batch_size:
+            #yield batch_sample, [batch_seg, batch_reg]
+            yield batch_sample, batch_label
+            
+            ind = 0   
+
+def my_loss(y_true, y_pred):
+
+    seg_true,reg_true = tf.split(y_true, [1, 9], 3)
+    seg_pred,reg_pred = tf.split(y_pred, [1, 9], 3)
+    
+    #ratio = 20*h*w/tf.reduce_sum(seg_true)
+    #weight1 = ((ratio-1)*seg_true + 1)/ratio
+    
+        
+    seg_loss = -tf.reduce_mean(tf.multiply(seg_true,tf.log(seg_pred+1e-8)) + tf.multiply(1-seg_true,tf.log(1-seg_pred+1e-8)))
+    #seg_loss = -tf.reduce_mean(
+    #    tf.multiply(tf.multiply(seg_true,tf.log(seg_pred)) + tf.multiply(1-seg_true,tf.log(1-seg_pred)), weight1))
+    
+    diff = tf.reduce_mean(tf.squared_difference(reg_true, reg_pred), axis=3, keep_dims=True)
+    reg_loss = tf.reduce_mean(tf.multiply(seg_true,diff))
+    
+    #total_loss = reg_loss
+    #total_loss = seg_loss
+    total_loss = seg_loss + reg_loss
+    return total_loss 
+
+
+if __name__ == '__main__':
+
+	data_dir = './extract_kiti/'
+
+	list_of_lidar, list_of_gtbox = list_of_data(data_dir)
+
+	# test on just one sample
+	list_of_lidar = [list_of_lidar[108]]
+	list_of_gtbox = [list_of_gtbox[108]]
+
+	model = fcn_model(input_shape = (64,256,2), summary = True)
+	opt = Adam(lr=1e-5)
+	model.compile(optimizer=opt, loss=my_loss)
+
+
+	model.fit_generator(generator=train_batch_generator(list_of_lidar, list_of_gtbox, batch_size = 1, data_augmentation = False),
+                        steps_per_epoch=1,
+                        epochs=300)
+
+	
+
--- util_func.py
+++ util_func.py
@@ -58,6 +58,218 @@ def cylindrical_projection(lidar,
     return view
 
 
+def is_in_box(point, box):
+    '''
+    point: tuple (x,y,z) coordinate
+    box: numpy array of shape (8,3)
+    return: True or False
+    '''
+    low = np.min(box[:,2])
+    high = np.max(box[:,2])
+    if (point[2] >= high) or (point[2]<=low):
+        return False
+    
+    v = point[:2] - box[0,:2]
+    v1 = box[1,:2] - box[0,:2]
+    v2 = box[3,:2] - box[0,:2]
+    
+    det1 = v[0]*v2[1] - v[1]*v2[0]
+    if det1 == 0:
+        return False
+    
+    det2 = v[0]*v1[1] - v[1]*v1[0]
+    if det2 == 0:
+        return False
+    
+    t1 = (v1[0]*v2[1] - v1[1]*v2[0])/det1
+    s1 = (v1[0]*v[1] - v1[1]*v[0])/det1
+    if (t1<=1) or (s1<=0):
+        return False
+    
+    t2 = (v2[0]*v1[1] - v2[1]*v1[0])/det2
+    s2 = (v2[0]*v[1] - v2[1]*v[0])/det2
+    if (t2<=1) or (s2<=0):
+        return False
+    
+    return True
+
+def in_which_box(point, boxes):
+    '''
+    return in which box the given point bolongs to, return 0 if the point doesn't belong to any boxes
+    '''
+    for i in range(len(boxes)):
+        if is_in_box(point, boxes[i]):
+            return i + 1
+    return 0
+
+
+
+def cylindrical_projection_with_labels(lidar,
+                                       gt_box3d,
+                                       ver_fov = (-24.4, 2.),#(-24.9, 2.), 
+                                       hor_fov = (-42.,42.), 
+                                       v_res = 0.42,
+                                       h_res = 0.33,
+                                       d_max = None):
+    '''
+    lidar: a numpy array of shape N*D, D>=3
+    ver_fov : angle range of vertical projection in degree
+    hor_fov: angle range of horizantal projection in degree
+    v_res : vertical resolusion
+    h_res : horizontal resolution
+    d_max : maximun range distance
+    
+    return : cylindrical projection (or panorama view) of lidar
+    '''
+    
+    x = lidar[:,0]
+    y = lidar[:,1]
+    z = lidar[:,2]
+    d = np.sqrt(np.square(x)+np.square(y))
+    
+    if d_max != None:
+        d[d>d_max] = d_max
+    
+    
+    theta = np.arctan2(-y, x)
+    phi = -np.arctan2(z, d)
+    
+    x_view = np.int16(np.ceil((theta*180/np.pi - hor_fov[0])/h_res))
+    y_view = np.int16(np.ceil((phi*180/np.pi + ver_fov[1])/v_res))
+    
+    #x_max = int(np.ceil((hor_fov[1] - hor_fov[0])/h_res))
+    #y_max = int(np.ceil((ver_fov[1] - ver_fov[0])/v_res))
+    x_max = 255
+    y_max = 63
+    
+    indices = np.logical_and( np.logical_and(x_view >= 0, x_view <= x_max), 
+                           np.logical_and(y_view >= 0, y_view <= y_max)  )
+    
+    x_view = x_view[indices]
+    y_view = y_view[indices]
+    z = z[indices]
+    d = d[indices]
+    d_z = [[d[i],z[i]] for i in range(len(d))]
+    
+    view = np.zeros([y_max+1, x_max+1, 2],dtype=np.float64)
+    view[y_view,x_view] = d_z
+    
+    encode_boxes = np.array([box_encoder(lidar[i], gt_box3d) for i in range(len(lidar))])
+    encode_boxes = encode_boxes[indices]
+    
+    box = np.zeros([y_max+1, x_max+1, 10],dtype=np.float32)
+    box[y_view,x_view] = encode_boxes
+    
+    # seg = np.zeros([y_max+1, x_max+1],dtype=np.float32)
+    # seg[y_view,x_view] = encode_boxes[:,0]
+    # seg = seg.reshape(y_max+1, x_max+1,1)
+    
+    # reg = np.zeros([y_max+1, x_max+1, 7],dtype=np.float32)
+    # reg[y_view,x_view] = encode_boxes[:,1:]
+    
+    return view, box
+
+def rotation(theta, point):
+    v = np.sin(theta)
+    u = np.cos(theta)
+    out = np.copy(point)
+    out[0] = u*out[0] + v*out[1]
+    out[1] = -v*out[0] + u*out[1]
+    return out
+
+def rotation_y(phi, point):
+    v = np.sin(phi)
+    u = np.cos(phi)
+    out = np.copy(point)
+    out[0] = u*out[0] + v*out[2]
+    out[2] = -v*out[0] + u*out[2]
+    return out
+
+
+def flip_rotation(theta, point):
+    v = np.sin(theta)
+    u = np.cos(theta)
+    out = np.copy(point)
+    out[0] = u*out[0] + v*out[1]
+    out[1] = v*out[0] - u*out[1]
+    return out
+
+
+
+def box_encoder(point, boxes):
+    '''
+        
+    '''
+    box_num = in_which_box(point, boxes)
+    #print(box_num)
+    if box_num==0:
+        return np.zeros(10)
+        
+    
+    box = boxes[box_num-1]
+    #print(box.shape)
+    
+    theta = np.arctan2(-point[1], point[0])
+    #print(theta*180/np.pi)
+    #phi = -np.arctan2(point[2], np.sqrt(point[0]**2 + point[1]**2) )
+    u0 = point[:3] - box[0] 
+    ru0 = rotation(-theta, u0)
+    
+    u1 = point[:3] - box[1] 
+    ru1 = rotation(-theta, u1)
+    #print('u= ', u)
+    u6 = point[:3] - box[6] 
+    ru6 = rotation(-theta, u6)
+    
+    #print('v= ', v)
+    #l1 = (box[3][0]-box[0][0])**2 + (box[3][1]-box[0][1])**2
+    #l2 = box[4][2]-box[0][2]
+    return np.array([1, ru0[0], ru0[1], ru0[2], ru1[0], ru1[1], ru1[2], ru6[0], ru6[1], ru6[2]])
+
+
+# def box_encoder(point, boxes):
+#     '''
+        
+#     '''
+#     box_num = in_which_box(point, boxes)
+#     #print(box_num)
+#     if box_num==0:
+#         return np.zeros(25)
+        
+    
+#     box = boxes[box_num-1]
+#     #print(box.shape)
+    
+#     theta = np.arctan2(-point[1], point[0])
+#     #print(theta*180/np.pi)
+#     phi = -np.arctan2(point[2], np.sqrt(point[0]**2 + point[1]**2) )
+    
+#     new_point = point[:3].reshape(1,3)
+#     offset = box - new_point
+#     offset = np.array([rotation_y(-phi, rotation(-theta,offset[i])) for i in range(8) ]).reshape(-1)
+#     out = np.ones(25)
+#     out[1:] = offset
+
+#     return out
+
+
+
+
+
+def augmentation(theta, flip, lidar, gtboxes):
+    if flip == 0:
+        out_lidar = np.array([rotation(theta, lidar[i]) for i in range(len(lidar))])
+        out_gtboxes = np.array([[rotation(theta, gtboxes[i,j,:]) for j in range(8)] for i in range(len(gtboxes))])
+    else:
+        out_lidar = np.array([flip_rotation(theta, lidar[i]) for i in range(len(lidar))])
+        out_gtboxes = np.array([[flip_rotation(theta, gtboxes[i,j,:]) for j in range(8)] for i in range(len(gtboxes))])
+   
+    return out_lidar, out_gtboxes
+
+    
+    
+
+
 def viz_mayavi_with_labels(points, boxes, view_boxes = True, vals="distance"):
     x = points[:, 0]  # x position of point
     y = points[:, 1]  # y position of point
