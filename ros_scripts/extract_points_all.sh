2b55a7c8a6341776b516a15acbf5386bff85a3b2
jaeil <parkjaeil0108@gmail.com>
2017-05-29 02:23:50 +0900

    ros script fixed 3

--- ros_scripts/extract_points_all.sh
+++ ros_scripts/extract_points_all.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
-sh extract_points.sh /home/parkjaeil0108/challenge/Didi-Release-2/Data/1/2.bag
-sh extract_points.sh /home/parkjaeil0108/challenge/Didi-Release-2/Data/1/3.bag
-sh extract_points.sh /home/parkjaeil0108/challenge/Didi-Release-2/Data/1/4_f.bag
-sh extract_points.sh /home/parkjaeil0108/challenge/Didi-Release-2/Data/2/1.bag
-sh extract_points.sh /home/parkjaeil0108/challenge/Didi-Release-2/Data/2/2.bag
-sh extract_points.sh /home/parkjaeil0108/challenge/Didi-Release-2/Data/2/3_f.bag
-sh extract_points.sh /home/parkjaeil0108/challenge/Didi-Release-2/Data/3/1.bag
-sh extract_points.sh /home/parkjaeil0108/challenge/Didi-Release-2/Data/3/2_f.bag
-sh extract_points.sh /home/parkjaeil0108/challenge/Didi-Release-2/Data/3/4.bag
-sh extract_points.sh /home/parkjaeil0108/challenge/Didi-Release-2/Data/Round_1_Test/19_f2.bag
+sh extract_points.sh /home/parkjaeil0108/didi_data/Didi-Release-2/Data/1/4_f.bag
+sh extract_points.sh /home/parkjaeil0108/didi_data/Didi-Release-2/Data/1/2.bag
+sh extract_points.sh /home/parkjaeil0108/didi_data/Didi-Release-2/Data/1/3.bag
+sh extract_points.sh /home/parkjaeil0108/didi_data/Didi-Release-2/Data/2/1.bag
+sh extract_points.sh /home/parkjaeil0108/didi_data/Didi-Release-2/Data/2/2.bag
+sh extract_points.sh /home/parkjaeil0108/didi_data/Didi-Release-2/Data/2/3_f.bag
+sh extract_points.sh /home/parkjaeil0108/didi_data/Didi-Release-2/Data/3/1.bag
+sh extract_points.sh /home/parkjaeil0108/didi_data/Didi-Release-2/Data/3/2_f.bag
+sh extract_points.sh /home/parkjaeil0108/didi_data/Didi-Release-2/Data/3/4.bag
+sh extract_points.sh /home/parkjaeil0108/didi_data/Didi-Release-2/Data/Round_1_Test/19_f2.bag
